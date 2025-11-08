bad4172a7311b924a484db8f3e7e68c14ed80e7a
Stevan Nikolic <stevannikolic1992@yahoo.com>
2025-11-08 18:28:26 +1100

    refactor Order component: extract addToCart function from form submit handler

--- complete-intro-to-react/padre-ginos/src/routes/order.lazy.jsx
+++ complete-intro-to-react/padre-ginos/src/routes/order.lazy.jsx
@@ -48,19 +48,24 @@ function Order() {
     fetchPizzaTypes();
   }, []);
 
+  // we could use this to run on the server and we could do something like this:
+  // function addToCart(formData) {
+  //   "use server";
+  //   sql(`INSERT INTO cart (user_id, pizza_type, size) VALUES ($1, $2)`, [
+  //     formData.pizza_type,
+  //     formData.size,
+  //   ]);
+  // }
+
+  function addToCart() {
+    setCart([...cart, { pizza: selectedPizza, size: pizzaSize, price }]);
+  }
+
   return (
     <div className="order-page">
       <div className="order">
         <h2>Create Order</h2>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            setCart([
-              ...cart,
-              { pizza: selectedPizza, size: pizzaSize, price },
-            ]);
-          }}
-        >
+        <form action={addToCart}>
           <div>
             <div>
               <label htmlFor="pizza-type">Pizza Type</label>
